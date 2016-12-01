62d12b89fa81909b3da6e7b6e74e87375daa10a6
dvad924 <dvad924@gmail.com>
2016-12-01 13:41:28 -0500

    added ctrl c listener to gracefully fall

--- demo.py
+++ demo.py
@@ -83,44 +83,46 @@ def outfacing(lock,receiver,finder=finder):
     cacheFlag = False
     label = None
     cv2.namedWindow('visual')
-    while cv2.waitKey(10) < 0 and not kill:
-        temp = processor.getFaces()
-        if receiver and receiver.poll(0.001):
-            signal = receiver.recv()
-            print(signal)
-            if signal.find('label:') >= 0:
-                label = signal[signal.find(':')+1:]
-                cacheFlag = True
-            if signal == 'STOPCACHE':
-                cacheFlag = False
-            if signal == 'KILL':
-                kill = True
-            if signal == 'Done Thinking':
-                clf,revmap = load_classifier(lock)
-                print ('HOT SWAPPED CLASSIFIER')
-        if temp is None:
-            continue
-        frame,fbbs,wfaces = temp
-        feats = extractor.getFeatures(wfaces)
-        pair = wfaces[0].shape
-        vizframe = np.zeros((pair[0],pair[1]*len(wfaces),3),dtype=np.uint8)
-        cv2.imshow('visual',frame)
-        clss = clf.predict_proba(feats)
-        if cacheFlag:
-            pairs = zip(feats,wfaces)
-            cacher.cachePairs(pairs,label)
-        i = 0
-        for face,probs in zip(wfaces,clss):
-            #retrieve the top 2 classes based on probabilty
-            top2 = probs.argsort()[-2:] 
-            for ix,cls in enumerate(top2):
-                cv2.putText(face,revmap[round(cls)]+':'+str(probs[cls]),
-                            (0,45+ix*50),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,255,0))
-                vizframe[:,i*96:(i+1)*96] = face
+    try:
+        while cv2.waitKey(10) < 0 and not kill:
+            temp = processor.getFaces()
+            if receiver and receiver.poll(0.001):
+                signal = receiver.recv()
+                print(signal)
+                if signal.find('label:') >= 0:
+                    label = signal[signal.find(':')+1:]
+                    cacheFlag = True
+                if signal == 'STOPCACHE':
+                    cacheFlag = False
+                if signal == 'KILL':
+                    kill = True
+                if signal == 'Done Thinking':
+                    clf,revmap = load_classifier(lock)
+                    print ('HOT SWAPPED CLASSIFIER')
+            if temp is None:
+                continue
+            frame,fbbs,wfaces = temp
+            feats = extractor.getFeatures(wfaces)
+            pair = wfaces[0].shape
+            vizframe = np.zeros((pair[0],pair[1]*len(wfaces),3),dtype=np.uint8)
+            cv2.imshow('visual',frame)
+            clss = clf.predict_proba(feats)
+            if cacheFlag:
+                pairs = zip(feats,wfaces)
+                cacher.cachePairs(pairs,label)
+            i = 0
+            for face,probs in zip(wfaces,clss):
+                #retrieve the top 2 classes based on probabilty
+                top2 = probs.argsort()[-2:] 
+                for ix,cls in enumerate(top2):
+                    cv2.putText(face,revmap[round(cls)]+':'+str(probs[cls]),
+                                (0,45+ix*50),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,255,0))
+                    vizframe[:,i*96:(i+1)*96] = face
 
-            i += 1
-        cv2.imshow('visuals',vizframe)
-    extractor.kill()
+                i += 1
+            cv2.imshow('visuals',vizframe)
+    finally:
+        extractor.kill()
     return 1
 
 if __name__ == '__main__':
